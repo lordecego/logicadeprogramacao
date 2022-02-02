992249a1b9a5d666bf80b50eb3d5cc8fa7a0f9f1
lordecego <natanaeldeo.souza@gmail.com>
2022-02-02 14:51:46 -0300

    mudança exercicio 1
    mudan

---
+++ index.js
@@ -0,0 +1,9 @@
+//1 – Crie uma função que retorna a string “Olá, ” concatenada com um argumento text (a ser passado para a função) e com ponto de exclamação “!” 
+//no final.
+
+function retornandostring(){
+
+    return "olá" + "!"
+}
+
+console.log(retornandostring())
