e9010dfa11e926afd7aab899a93dd6aa1971f3c7
lordecego <natanaeldeo.souza@gmail.com>
2022-02-02 15:10:35 -0300

    exercicio de calculadora
    exercicio de calculadora

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ console.log(stringretorno())
 
 
 
+//2 – Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração, 
+//multiplicação e divisão desses valores.
+
+function calculadora(valor1,valor2){
+    console.log(`A Soma é :${valor1 + valor2}`)
+    console.log(`A Subtração é :${valor1 - valor2}`)
+    console.log(`A Multiplicação é :${valor1 * valor2}`)
+    console.log(`A Divisão é :${valor1 / valor2}`)
+}
+
+calculadora(2,4)
