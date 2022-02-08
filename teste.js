eb182738bb8b498eecdc03fadfc63e11b2b7e88f
lordecego <natanaeldeo.souza@gmail.com>
2022-02-08 16:57:23 -0300

    boelano !valor
    typeof valor

--- teste.js
+++ teste.js
@@ -100,3 +100,15 @@ numérico, o retorno será o número inverso. Por exemplo, se for fornecido 1, o
 de entrada não for de nenhum dos tipo acima, retorne "booleano ou número esperados, mas o parâmetro é do
 tipo ...". */
 
+function inverso(valor){
+    const tipo= typeof valor
+
+    if(tipo =="boolean") return !valor
+    else if(tipo =="number")return -valor
+    else
+        return `booleano ou número esperados, mas o parâmetro é do tipo ${tipo}`
+}
+
+console.log(inverso(true))
+console.log(inverso(6))
+console.log(inverso("2000"))
