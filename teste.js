1438fbe53535bcdec7cc0337522426b71e0f11f2
lordecego <natanaeldeo.souza@gmail.com>
2022-02-08 17:25:52 -0300

    update exercicio 7
    exercicio 7

--- teste.js
+++ teste.js
@@ -126,3 +126,10 @@ console.log(inverso("2000"))
 
 
 
+/*Crie uma função que receba quatro números como parâmetro (numero, minimo, maximo, inclusivo) e retorne se
+o parâmetro numero (o primeiro) está entre minimo e maximo. Quando o parâmetro inclusivo for true, tenha
+"entre" como inlusivo, ou seja, considerando se numero é igual a minimo ou a maximo. Caso o parâmetro
+inclusivo não seja informado, seu valor padrão deverá ser false, portanto, a lógica será exclusiva, não
+considerando se numero é igual a minimo ou a maximo. */
+
+
